9133bed21cd37dc9a0ab6e0bee127bf972a7394d
Zenquan <1847867150@qq.com>
2022-06-21 22:59:59 +0800

    fix: 修改mock文件

--- mock/menus.ts
+++ mock/menus.ts
@@ -3,35 +3,34 @@ import EN_MENUS from './lang/en.json';
 
 const getMenus = (roleType: string, lng: string) => {
   let result: {
-    ret: string,
-    msg: string
+    ret: string;
+    msg: string;
     data: {
-      menus: any
-    }
+      menus: any;
+    };
   } = {
     ret: '0',
     msg: '成功',
     data: {
-      menus: []
+      menus: [],
     },
-  }
+  };
 
   if (roleType === '1') {
-    if (lng === 'cn') {
-      result.data.menus = CN_MENUS.admin
+    if (lng === 'zh-CN') {
+      result.data.menus = CN_MENUS.admin;
     } else {
-      result.data.menus = EN_MENUS.admin
+      result.data.menus = EN_MENUS.admin;
     }
-
   } else if (roleType === '2') {
-    if (lng === 'cn') {
-      result.data.menus = CN_MENUS.zenquan
+    if (lng === 'zh-CN') {
+      result.data.menus = CN_MENUS.zenquan;
     } else {
-      result.data.menus = EN_MENUS.zenquan
+      result.data.menus = EN_MENUS.zenquan;
     }
   }
 
-  return result
-}
+  return result;
+};
 
 export default getMenus;
